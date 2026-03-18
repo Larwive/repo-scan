cfc82632ea03b28186aab088e29ca43ed75d69ec
Larwive <larwive@gmail.com>
2026-03-18 21:26:52 +0100

    Change clone path to fixed directory in home directory, create directory for scan results

--- include/SemgrepScanner.hpp
+++ include/SemgrepScanner.hpp
@@ -19,7 +19,7 @@ public:
   }
 
   Issue scan(const std::string &repoPath) override {
-    std::string semgrep_cmd = "semgrep scan --json " + repoPath;
+    std::string semgrep_cmd = "semgrep scan --json -o " + repoPath + "/repo-scan/semgrep.json " + repoPath;
     std::system(semgrep_cmd.c_str());
 
     Issue semgrep_result = {
--- src/orchestrator.cpp
+++ src/orchestrator.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <memory>
 
 #include "SemgrepScanner.hpp"
@@ -8,12 +9,20 @@ ScanOrchestrator::ScanOrchestrator() {
   auto semgrep = std::make_unique<SemgrepScanner>();
   if (semgrep->isAvailable()) {
     scanners.push_back(std::move(semgrep));
+    std::cout << "Semgrep scanner added to queue.\n";
+  } else {
+    std::cout << "Semgrep scanner skipped.\n";
   }
 }
 
 Report ScanOrchestrator::run(const std::string &repoUrl) {
-  std::string repo_path = std::string(getenv("HOME")) + "/tmp/repo-scan";
-  std::string cmd = "git clone " + repoUrl + " " + repo_path;
+  std::string repo_scan_path = std::string(getenv("HOME")) + "/repo-scanned/";
+  std::string mkdir_command = "mkdir -p " + repo_scan_path;
+  std::system(mkdir_command.c_str());
+
+  std::string repo_path =
+      repo_scan_path + repoUrl.substr(repoUrl.find_last_of("/") + 1);
+  std::string cmd = "cd " + repo_scan_path + ";git clone " + repoUrl;
   std::system(cmd.c_str());
 
   Report report;
